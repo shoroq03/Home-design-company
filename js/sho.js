f25814711deb76d6f6e2dd268b0727674e3da524
shoroq03 <shoroq274@gmail.com>
2019-09-25 15:59:31 +0300

    htmlcssjava

---
+++ js/sho.js
@@ -0,0 +1,11 @@
+var helpYou =confirm("can help you to design your house or choosing for you");
+var output;
+
+if (helpYou == true) {
+    output ='contant us to help you';
+    
+} else
+ {
+    output ='visit our  website and hope you enjoy';
+}
+    document.write('<h1>'+output+'</h1>')
