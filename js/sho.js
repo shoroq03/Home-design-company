6811dbe3627b102b9899a9ea2d9cec488cb07bef
shoroq03 <shoroq274@gmail.com>
2019-09-26 14:13:06 +0300

    why

--- js/sho.js
+++ js/sho.js
@@ -8,3 +8,19 @@ if (helpYou == true) {
     output = 'visit our  website and hope you enjoy';
 }
 document.write('<h1>' + output + '</h1>')
+
+var order = comfirm('buy above 1000$ for free dilivery');
+function buyAbove(money){ 
+    // input
+  
+    var dilivery;
+    if (money>=1000) {
+        var dilivery = ('free dilivery');
+       
+    } else {
+        var dilivery = ('enjoy for buying');
+    }
+    //output
+
+    document.write('<h3>' + buyAbove() + '<h3>');
+}
